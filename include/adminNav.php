aa45bba87103736b1b8b399ac7a6d67fac7f136f
Mitsune91 <tho.giotti@gmail.com>
2018-10-21 17:06:16 +0200

    Update include/adminNav.php
    Menu du mode administrations

---
+++ include/adminNav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-dark bg-dark  text-white">
+    <ul class="navbar-nav nav-fill w-100 flex-md-row text-white">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=writeNewPost">Ecrire un nouvel article</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=managePosts">Gestion des articles</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=manageComments">Gestion des commentaires</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=manageUsers">Liste des membres</a>
+        </li>
+
+    </ul>
+</nav>
+</div>
